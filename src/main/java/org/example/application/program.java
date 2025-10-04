1a2253bc49132a910d15dc42b964425ddd56043f
tempestaGui <guilhermeftempesta@gmail.com>
2025-10-04 16:15:36 -0300

    Configurando classe entity e inserindo no banco com persist

--- src/main/java/org/example/application/program.java
+++ src/main/java/org/example/application/program.java
@@ -2,13 +2,26 @@ package org.example.application;
 
 import org.example.entity.Pessoa;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class program {
     public static void main(String[] args) {
-        Pessoa p1 = new Pessoa("Gui", 1, "Gui@gmail.com");
-        Pessoa p3 = new Pessoa("Gusta", 2, "Gusta@gmail.com");
-        Pessoa p2 = new Pessoa("Gab", 3, "Gab@gmail.com");
-        System.out.println(p1);
-        System.out.println(p3);
-        System.out.println(p2);
+        Pessoa p1 = new Pessoa("Gui", null, "Gui@gmail.com");
+        Pessoa p3 = new Pessoa("Gusta", null, "Gusta@gmail.com");
+        Pessoa p2 = new Pessoa("Gab", null, "Gab@gmail.com");
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+        EntityManager em = emf.createEntityManager();
+
+        em.getTransaction().begin(); //iniciar uma transaçao com o bd
+
+        em.persist(p1); //pega um obj e salva no banco
+        em.persist(p2);
+        em.persist(p3);
+
+        em.getTransaction().commit(); //confirma transaçoes
+        System.out.println("Pessoa Criado com sucesso!");
     }
 }
--- src/main/java/org/example/entity/Pessoa.java
+++ src/main/java/org/example/entity/Pessoa.java
@@ -1,12 +1,18 @@
 package org.example.entity;
 
+import javax.persistence.*;
 import java.io.Serializable;
 
+@Entity
 public class Pessoa implements Serializable {
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
     private String nome;
+
     private String email;
 
     public Pessoa() {}
