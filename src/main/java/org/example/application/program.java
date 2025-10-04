da628c2331a5a0e51a1f8aa6741dce476fbea6d3
tempestaGui <guilhermeftempesta@gmail.com>
2025-10-04 16:19:02 -0300

    Buscando pessoa do bd com find

--- src/main/java/org/example/application/program.java
+++ src/main/java/org/example/application/program.java
@@ -8,20 +8,28 @@ import javax.persistence.Persistence;
 
 public class program {
     public static void main(String[] args) {
-        Pessoa p1 = new Pessoa("Gui", null, "Gui@gmail.com");
-        Pessoa p3 = new Pessoa("Gusta", null, "Gusta@gmail.com");
-        Pessoa p2 = new Pessoa("Gab", null, "Gab@gmail.com");
+//        Pessoa p1 = new Pessoa("Gui", null, "Gui@gmail.com");
+//        Pessoa p3 = new Pessoa("Gusta", null, "Gusta@gmail.com");
+//        Pessoa p2 = new Pessoa("Gab", null, "Gab@gmail.com");
 
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
         EntityManager em = emf.createEntityManager();
 
-        em.getTransaction().begin(); //iniciar uma transaçao com o bd
+//        em.getTransaction().begin(); //iniciar uma transaçao com o bd
+//
+//        em.persist(p1); //pega um obj e salva no banco
+//        em.persist(p2);
+//        em.persist(p3);
+//
+//        em.getTransaction().commit(); //confirma transaçoes
 
-        em.persist(p1); //pega um obj e salva no banco
-        em.persist(p2);
-        em.persist(p3);
+        Pessoa p = em.find(Pessoa.class, 1);
+        System.out.println(p);
 
-        em.getTransaction().commit(); //confirma transaçoes
         System.out.println("Pessoa Criado com sucesso!");
+        em.close();
+        emf.close();
+
+
     }
 }
